8e9189692b541183ab136eb12064e126a6ead226
Freddy Pringle <freddyjepringle@gmail.com>
2023-07-25 14:46:32 +0200

    remove version bounds; add default.nix

---
+++ default.nix
@@ -0,0 +1,15 @@
+let
+  pkgs = import <nixpkgs> { }; # pin the channel to ensure reproducibility!
+in
+pkgs.haskellPackages.developPackage {
+  root = ./.;
+  modifier = drv:
+    pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+      [ cabal-install
+        haskell-language-server
+        hlint
+        fourmolu
+        ghcid
+        zlib
+      ]);
+}
